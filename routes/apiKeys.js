4adb50dd934f3f86b20838da3434b5a9172522d6
Max Häggman <max.haggman@edu.cmeducations.se>
2023-05-11 10:51:25 +0200

    remove second delete endpoint

--- routes/apiKeys.js
+++ routes/apiKeys.js
@@ -24,27 +24,17 @@ router.delete('/', (req, res) => {
     res.json(keys)
 })
 
-router.delete('/:key', (req, res) => {
-    const userKey = req.params.key;
-    const key = keys.find( key => key === userKey)
-
-    if (!key) {
-        return res
-        .status(404)
-        .json({ message: 'No key could be found'})
-    }
-
-    const newKeys = keys.filter( key => key !== userKey)
-    keys = newKeys;
-
-    res.json(keys)
-})
-
 router.post('/', (req, res) => {
     
     const keyObject = req.body
 
     const keyString = keyObject.key;
+
+    if (keys.includes(keyString)) {
+        return res
+        .status(400)
+        .json({message: 'That key already exists. You can either create a new one or use that key for future requests'})
+    }
 
     keys = [...keys, keyString]
     
